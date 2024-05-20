d2a9cd0ac6a98d7d6548f80e81840d04564bcc56
Jack Halverson <jhalverson4838@xmail.dixietech.edu>
2024-05-20 11:12:35 -0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,6 @@ var bill = parseFloat(prompt("Enter the Bill amount"));
 var tipPercent = parseFloat(prompt("Enter the percentage to tip"));
 
 var tipAmount = (bill * tipPercent) / 100;
-
 var totalBill = bill + tipAmount;
 
 alert(totalBill.toFixed(2))
