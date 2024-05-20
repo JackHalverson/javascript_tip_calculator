ad082eeb86435124190e7f19e30f0da460ee4f18
Jack Halverson <jhalverson4838@xmail.dixietech.edu>
2024-05-20 11:09:13 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# javascript_tip_calculator
+ 
---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+var bill = parseFloat(prompt("Enter the Bill amount"));
+var tipPercent = parseFloat(prompt("Enter the percentage to tip"));
+
+var tipAmount = (bill * tipPercent) / 100;
+
+var totalBill = bill + tipAmount;
+
+alert(totalBill.toFixed(2))
